6ec5d2d38167be4efe374df2119c0b65b7e6c517
Rais-Bouamara <rais.bouamara@gmail.com>
2026-05-06 09:45:13 +0200

    feg

---
+++ conditiion.js
@@ -0,0 +1,135 @@
+//exercice 1
+console.log(10 === 10)
+
+
+//exercice 2
+console.log("Hello" !== "hello")
+
+
+//exercice 3
+
+console.log(8 > 12)
+
+
+let b = 8;
+let c = 6;
+
+if (b === c) {
+    console.log("c'est pareil")
+} else if (b > c) {
+    console.log("b est superieur a c")
+} else {
+    console.log("b est inferieur a c")
+}
+
+let connect = true
+
+if (connect === true) {
+    console.log("l'utilisateur est connecté")
+} else {
+    console.log("l'utilisateur n'est pas connecté")
+}
+
+
+
+//exercice 5
+let meteo = ("soleil")
+
+if (meteo === "neige") {
+    console.log("met tes bottes")
+} else if (meteo === "pluie") {
+    console.log("N’oublie pas ton parapluie")
+} else if (meteo === "soleil") {
+    console.log("Prends une casquette")
+} else {
+    console.log("reste chez toi !")
+}
+
+//exercice 6
+let age1 = 14
+
+if (age1 < 12) {
+    console.log("enfant")
+} else if (age1 >= 12 && age1 <= 18) {
+    console.log("adolescent")
+} else if (age1 > 18) {
+    console.log("adulte")
+}
+
+//exercice 7
+let cotéa = 13
+let cotéb = 13
+let cotéc = 13
+
+if (cotéa === cotéb && cotéb === cotéa && cotéc === cotéc) {
+    console.log("C'est un triangle équilatéral")
+} else if (cotéa !== cotéb && cotéa === cotéc && cotéb === cotéc) {
+    console.log("C'est un triangle isocèle")
+} else {
+    console.log("C'est un triangle quelconque")
+}
+
+//exercice 8
+let achat = 100
+let programme = true
+let age2 = 18
+
+
+
+//role :détermine si une personne est éligible à une réduction spéciale
+//parametre :avoir plus de 18 ans;membre du programme de fidélité;avoir effectué un achat supérieur à 100 euros;
+//return: 2 message (reducion accordé et reducution refuser)
+
+function eligibilité(age2, programme, achat) {
+
+    if (age2 < 18 && programme == true || achat < 100) {
+        console.log("Réduction accordée")
+
+    } else {
+        console.log("Réduction refusée")
+
+    }
+    return reponse1 && reponse2
+}
+
+let reponse1 = "Réduction accordée"
+let reponse2 = "Réduction refusée"
+
+//correction
+
+function reduction(age, membre, achat) {
+    //si la personne a 18 ans
+    if (age > 18) {
+        //si la personne est membre ou a fait plus de 100 euros dachat
+        if (membre === true || achat > 100){
+            //reduction accordée
+            return "reduction accordée"
+    } else {
+        //si non
+        //reduction refusé
+        return "reduction refusée"
+
+    }
+}
+else {
+        //sinon :
+        //reduction refusé
+        return "reduction refusée"
+    }
+}
+
+let ticket = reduction(19, true, 80)
+console.log(ticket)
+
+//Exercice du feu tricolor
+let signalisation = vert
+
+if(signalisation === vert){
+    console.log("Je passe !")
+}else if (signalisation === orange){
+    console.log("Je freine !")
+}else if (signalisation === rouge){
+    console.log("Je m'arrête !")
+}else {
+    console.log("Danger : feu défectueux !")
+}
--- exofonction.js
+++ exofonction.js
@@ -1,24 +1,25 @@
+/*
 //exercice 1
 //role: calculer une moyenne
 //parametre: quatre moyennes
 //return: la moyenne
 
-function calculDeMoyenne(mathematique,francais,italien,chimie){
+function calculDeMoyenne(mathematique, francais, italien, chimie) {
     return (mathematique + francais + italien + chimie) / 4;
 }
 
 
-console.log(`la moyenne de classe est ${calculDeMoyenne(12,16,15,18)}`)
+console.log(`la moyenne de classe est ${calculDeMoyenne(12, 16, 15, 18)}`)
 
 //exercice 2
 //role:créer une fonction mettant à jour le stock de pommes du magasin
 //parametre: rien
 //return: rien
 
-let stockDePomme=4;
+let stockDePomme = 4;
 
-function decremente(){
-     stockDePomme--
+function decremente() {
+    stockDePomme--
 }
 
 decremente()
@@ -32,9 +33,9 @@ console.log(`le stock de pomme est de ${stockDePomme}`)
 //return: rien
 let phrases = "djsftgqdsjfyqhiuyfakygfkqhf,h;zyghfijlxu"
 
-function TrouveLettre(phrases){
-let pos = phrases.length-1;
-console.log(phrases[pos])
+function TrouveLettre(phrases) {
+    let pos = phrases.length - 1;
+    console.log(phrases[pos])
 }
 
 TrouveLettre(phrases)
@@ -44,11 +45,11 @@ TrouveLettre(phrases)
 //role: remplacer le produit bubble tea par un autre 
 //parametre: le nouveau produit et la liste initial 
 //return: rien
-function NouvelleOffre(nouveauProduit, listeInitiale){
+function NouvelleOffre(nouveauProduit, listeInitiale) {
     console.log(listeInitiale.replace("bubble tea", nouveauProduit))
 }
 
-NouvelleOffre("matcha","café,bubble tea,tisane")
+NouvelleOffre("matcha", "café,bubble tea,tisane")
 
 
 
@@ -56,9 +57,9 @@ NouvelleOffre("matcha","café,bubble tea,tisane")
 //parametre:Les balises a ajouter   
 //return:Rien
 
-function AjtBalise(balise){
+function AjtBalise(balise) {
 
-    document.querySelector("body").innerHTML+=balise
+    document.querySelector("body").innerHTML += balise
 }
 
 AjtBalise("<H1>Coucou</H1>")
@@ -70,19 +71,108 @@ AjtBalise(`<img src="" alt=""></img>`)
 //return:Rien
 
 
-function afficherNom(Nom,Prenom){
-    AjtBalise(`<p>${Prenom +" "+Nom}</p>`)
+function afficherNom(Nom, Prenom) {
+    AjtBalise(`<p>${Prenom + " " + Nom}</p>`)
 }
 
-afficherNom("Raïs","Bouamara")
+afficherNom("Raïs", "Bouamara")
 
 //Exercice 5
 //role: Verifier une adress mail
 //parametre: une adress mail
 //return: si vrai si ladress mail contient un @
 
-function VerifMails(mail){
-    return mail.includes("@")
+function VerifMails(mail) {
+    console.log(mail.includes("@"))
 }
 
-console.log()
+VerifMails("rais.bouamara@gmail.com")
+
+
+//Exercice 6
+//role: retournera toujours la somme des deux valeurs renseignées 
+//parametre: deux valeurs
+//return: la somme des 2 valeurs
+
+function addition(a, b) {
+    return a + b
+};
+let resultat = addition(5, 4);
+
+console.log(resultat);
+
+
+//Exercice 7
+//role: posera une question en utilisant la fonction "prompt"
+//parametre: un prompt
+//return: retourner la reponse dans la console
+
+function poseUneQuestion(q) {
+    return prompt(q)
+
+}
+
+let reponse = poseUneQuestion("ca va ?")
+alert(reponse)
+
+//Exercice 8 - BONUS
+//role: créer une fonction qui convertit un montant en euros en dollars.
+//parametre:un montant en euros
+//return: retourner le résultat de la conversion.
+
+function conversion(prix, taux) {
+    return prix * taux;
+}
+
+let resultat1 = conversion(3, 1.17)
+console.log(`le prix convertit est de ${resultat1} dollars`);
+
+//BONUS
+//role: demander un prix en taux de change a l'utilisateur et convertit le prix
+//parametre:prix et taux de change
+//return: retourner prix convertit
+
+function convertisseur() {
+    //demande le prix a convertir
+    let prix =parseFloat(prompt("quel prix veut tu convertir ?"))
+
+    //deamande la taux
+    let taux =parseFloat(prompt("a quel taux ?"))
+    
+    //convertit
+return prix * taux
+
+    //retourner le resultat
+
+}
+
+let prixConvertit =convertisseur();
+alert(`le prix convertit est de ${prixConvertit} dollars`)
+
+
+//BONUS
+//role: faire 4 prompt qui demande les notes
+//parametre:
+//return: retourner la moyenne
+
+function CalculMoyenne() {
+    //demande la note de francais
+    let francais =parseFloat(prompt("quel est la note de francais ?"))
+
+    //deamande la note histoire
+    let histoire =parseFloat(prompt("quel est la note d'histoire ?"))
+
+     //deamande la note math
+    let math =parseFloat(prompt("quel est la note de math ?"))
+
+     //deamande la note svt
+    let svt =parseFloat(prompt("quel est la note de svt ?"))
+
+
+    //calcul de moyenne
+    return (francais + histoire + math + svt)/4 
+}
+
+let MoyenneEleve =CalculMoyenne();
+alert(`la moyenne de l'éléve est de ${MoyenneEleve} sur 20 !`)
+*/
